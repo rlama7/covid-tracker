03888a1d1942471f0d801fc5dc219da930776189
Ratna Lama <royalray7@gmail.com>
2022-03-28 16:14:23 -0700

    configured tailwindcss

--- vue-covid-tracker/tailwind.config.js
+++ vue-covid-tracker/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: { content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'] },
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
