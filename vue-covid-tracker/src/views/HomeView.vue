d184469783d23345188cb124a5c292e3cd96d7a2
Ratna Lama <royalray7@gmail.com>
2022-03-28 16:55:19 -0700

    updated fetch request

--- vue-covid-tracker/src/views/HomeView.vue
+++ vue-covid-tracker/src/views/HomeView.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="home">
+  <main class="home">
     Hello World
-  </div>
+  </main>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
-  components: {
-    
+  components: {},
+  data(){
+    return{
+      loading:true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods:{
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created(){
+    const data = await this.fetchCovidData()
+    // console.log(data)
+    this.dataDate = data.dataDate
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
   }
 }
 </script>
