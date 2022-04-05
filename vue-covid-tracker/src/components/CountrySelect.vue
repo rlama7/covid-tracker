d7a4ec67769e588a4be95a67acec5d29c7562d44
Ratna Lama <royalray7@gmail.com>
2022-04-04 21:27:09 -0700

    updated clear button

--- vue-covid-tracker/src/components/CountrySelect.vue
+++ vue-covid-tracker/src/components/CountrySelect.vue
@@ -1,6 +1,8 @@
 <template>
-<select @change="onChange()"
-v-model="selected" class="form-select mt-10 block w-full border p-3 rounded">
+<select 
+@change="onChange()"
+v-model="selected" 
+class="form-select mt-10 block w-full border p-3 rounded">
 	<option value="0">Select Country</option>
 	<option v-for="country in countries" :value="country.ID">
 		{{country.Country}}
