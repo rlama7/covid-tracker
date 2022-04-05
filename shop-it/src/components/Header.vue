e45e4c05d553dca1d88823cdd6aef6656a60077f
Ratna Lama <royalray7@gmail.com>
2022-04-05 14:03:43 -0700

    initiated shop it list app

---
+++ shop-it/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <div>Shop It List -> Header</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
---
+++ shop-it/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+   Hello World -> Home View
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
